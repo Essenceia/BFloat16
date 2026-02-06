c11a5d779172f04e3d7f42836aa2d046ae1b5f73
Julia Desmazes <julia.desmazes@gmail.com>
2026-02-06 13:39:01 -0600

    linting add module

--- src/bf16_add.v
+++ src/bf16_add.v
@@ -22,7 +22,7 @@ module bf16_add #(
 
 	output wire s_o,
 	output wire [E-1:0] e_o,
-	output wire [M-1:0] m_o,
+	output wire [M-1:0] m_o
 );
 
 /* Internally the addition is performed on 
@@ -42,9 +42,9 @@ wire         eab_diff_carry, eba_diff_carry;
 assign {eab_diff_carry, eab_diff} = ea_i - eb_i;
 assign {eba_diff_carry, eba_diff} = eb_i - ea_i;
 
-assign exy_diff = ~eab_diff_carry? eab_diff ? eba_diff;
-assign {ex, ey} = ~eab_diff_carry? {ea_i, eb_i} ? {eb_i, ea_i}; 
-assign {mx, my} = ~eab_diff_carry? {ma_i, mb_i} ? {mb_i, ma_i}; 
+assign exy_diff = ~eab_diff_carry? eab_diff: eba_diff;
+assign {ex, ey} = ~eab_diff_carry? {ea_i, eb_i}: {eb_i, ea_i}; 
+assign {mx, my} = ~eab_diff_carry? {ma_i, mb_i}: {mb_i, ma_i}; 
 
 /* --------
    far path  
@@ -59,11 +59,11 @@ wire         shift_underflow;
 
 always @(*) begin
 	case(exy_diff[2:0])
-		2: my_shift_lite = {1'b1, my[M-1:2];
-		3: my_shift_lite = {1'b0, 1'b1, my[M-1:3];
-		4: my_shift_lite = {2'b0, 1'b1, my[M-1:4];
-		5: my_shift_lite = {3'b0, 1'b1, my[M-1:5];
-		6: my_shift_lite = {4'b0, 1'b1, my[6];
+		2: my_shift_lite = {1'b1, my[M-1:2]};
+		3: my_shift_lite = {1'b0, 1'b1, my[M-1:3]};
+		4: my_shift_lite = {2'b0, 1'b1, my[M-1:4]};
+		5: my_shift_lite = {3'b0, 1'b1, my[M-1:5]};
+		6: my_shift_lite = {4'b0, 1'b1, my[6]};
 		7: my_shift_lite = {5'b0, 1'b1};
 		default: my_shift_lite = {M-1{1'bX}};// handled by close path, don't care, letting the optimization do what it wants
 	endcase
@@ -95,11 +95,11 @@ always @(*) begin
 		2'b00: begin // divide by 2
 			{er_norm_carry, er_norm} = er - {{E-2{1'b0}},1'b1};
 			mr_norm = {m_r[M-1:0], 1'b0}; // should I have kept the guard bit to inject it here?
-		begin
+		end
 		2'b1X: begin // multiply by 2 
 			{er_norm_carry, er_norm} = er + {{E-2{1'b0}},1'b1};
 			mr_norm = {1'b0, m_r[M+1:1]}; 
-		begin
+		end
 		default: begin
 			er_norm_carry = 1'b0;
 			er_norm = ex;
@@ -144,7 +144,7 @@ wire [LZC_W-1:0] zero_cnt;
 wire [M+1:0] mz_cp_norm; 
 
 always_comb @(*) begin
-	case(msb_one_idx) begin
+	case(msb_one_idx) 
 		'd0: mz_cp_norm = mxy_cp_abs_diff; // no cancellation 
 		'd1: mz_cp_norm = {mxy_cp_abs_diff[M:0], 1'b0}; 
 		'd2: mz_cp_norm = {mxy_cp_abs_diff[M-1:0], 2'b0}; 
@@ -155,6 +155,7 @@ always_comb @(*) begin
 		'd7: mz_cp_norm = {mxy_cp_abs_diff[M-6:0], 7'b0}; 
 		'd8: mz_cp_norm = {1'b1, 8'b0}; //only 1 left 
 		'd9: mz_cp_norm = {1'b1, {M+1{1'b0}}}; // full cancellation, nothing is left
+	endcase
 end
 
 // normalize exponent
@@ -180,7 +181,7 @@ assign fp_sel = |exy_diff[E-1:1]; // diff > 1
 //  - +/i inf
 
 // return
-assign s_o = // TODO
+assign s_o = 1'b0;// TODO
 assign e_o = fp_sel ? er_norm : ez_cp_norm;
 assign m_o = fp_sel ? mr_norm[M-1:0]: mz_cp_norm[M:1];
 
