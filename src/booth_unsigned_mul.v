be297012de50b64c9145ae36a042ba1a3d3f3221
Julia Desmazes <julia.desmazes@gmail.com>
2026-02-23 18:38:53 -0600

    add placeholder module

---
+++ src/booth_unsigned_mul.v
@@ -0,0 +1,13 @@
+/* 
+ Placeholder module, actual mul will be inferred by yosys abc
+*/ 
+module booth_unsigned_mul #(
+	parameter W = 8
+)(
+	input wire [W-1:0] x_i,
+	input wire [W-1:0] y_i,
+	output wire [2*W-1:0] z_o
+);
+
+assign z_o = x_i * y_i; 
+endmodule
